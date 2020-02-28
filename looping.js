5e09c390d367f0815242552b328dcc50d7cb2df9
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-02-28 23:31:47 +0700

    menambah file looping, perulangan, dan string

---
+++ looping.js
@@ -0,0 +1,25 @@
+var rows1 = 5;
+var cols = 5;
+var rows3 = 5;
+
+// //1. Menyusun Barisan Bintang
+// for (var i = 0; i < rows1; i++) {
+//   var asterisk = "*";
+//   console.log(asterisk);
+// }
+
+// //2. Menyusun Barisan Bintang Dengan Nested Looping
+// for (var i = 0; i < rows1; i++) {
+// var asterisk ="";
+//   for (var j = 0; j < cols; j++) {
+//     asterisk += "*";
+//   }
+//   console.log(asterisk);
+// }
+
+//3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+asterisk = "";
+for (var i = 0; i < rows3; i++) {
+  asterisk+="*";
+  console.log(asterisk);
+}
---
+++ perulangan.js
@@ -0,0 +1,91 @@
+var kalimat = "I love coding";
+var kalimat2 = "I will become fullstack developer";
+var angka = 0;
+var limit = 20;
+var limitSeratus =100;
+
+// //1. Melakukan Looping Menggunakan while
+// //Looping pertama
+// while (angka < limit ) {
+//   angka+=2;
+//   console.log(angka + " - " + kalimat);
+// }
+
+// //Looping kedua
+// while (limit > angka) {
+//   console.log(limit + " - " + kalimat2);
+//   limit -=2;
+// }
+
+// //2. Looping Menggunakan For
+// //LOOPING PERTAMA
+// for (var i = 1; i <= limit; i++) {
+//   console.log(i + " - " + kalimat);
+// }
+
+// //LOOPING KEDUA
+// for (var i = 20; i > angka; i--) {
+//   console.log(i + " - " + kalimat2);
+// }
+
+// //3. Angka Ganjil dan Genap
+// //ganjil-genap
+// //kelipatan 2
+// for (var i = 1; i <= limitSeratus; i+= 2) {
+//   if (i%2 === 0) {
+//     console.log(i + " GENAP");
+//   }
+//   else {
+//     console.log(i + " GANJIL");
+//   }
+// }
+
+// //kelipatan 5
+// for (var i = 1; i <= limitSeratus; i+= 5) {
+//   if (i%2 === 0) {
+//     console.log(i + " GENAP");
+//   }
+//   else {
+//     console.log(i + " GANJIL");
+//   }
+// }
+
+// //kelipatan 9
+// for (var i = 1; i <= limitSeratus; i+= 9) {
+//   if (i%2 === 0) {
+//     console.log(i + " GENAP");
+//   }
+//   else {
+//     console.log(i + " GANJIL");
+//   }
+// }
+
+// //kelipatan 3 dengan pertambahan 2
+// for (var i = 1; i <= limitSeratus; i+=2) {
+//   if (i%3 === 0) {
+//     console.log(i + " KELIPATAN 3");
+//   }
+//   else {
+//     console.log("");
+//   }
+// }
+
+// //kelipatan 6 dengan pertambahan 5
+// for (var i = 1; i <= limitSeratus; i+=5) {
+//   if (i%6 === 0) {
+//     console.log(i + " KELIPATAN 6");
+//   }
+//   else {
+//     console.log("");
+//   }
+// }
+
+// //kelipatan 10 dengan pertambahan 9
+// for (var i = 1; i <= limitSeratus; i+=9) {
+//   if (i%10 === 0) {
+//     console.log(i + " KELIPATAN 10");
+//   }
+//   else {
+//     console.log("");
+//   }
+// }
